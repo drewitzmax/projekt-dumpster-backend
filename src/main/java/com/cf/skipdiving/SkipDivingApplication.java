34e4464c8f46d119918c7bed1852ef8ce83c9b6d
drewitzmax <drwitz.max@googlemail.com>
2020-08-03 11:42:04 +0200

    SpringBoot base App

---
+++ src/main/java/com/cf/skipdiving/SkipDivingApplication.java
@@ -0,0 +1,13 @@
+package com.cf.skipdiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipDivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipDivingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cf/skipdiving/SkipDivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.skipdiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkipDivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
