122c6d4565a1609b82e8c308c6bf26f1e0e312c3
drewitzmax <drwitz.max@googlemail.com>
2020-08-05 08:54:29 +0200

    providing getAll endpoint

--- src/main/java/com/cf/skipdiving/rest/ProviderController.java
+++ src/main/java/com/cf/skipdiving/rest/ProviderController.java
@@ -5,11 +5,14 @@ import com.cf.skipdiving.jpa.entity.Provider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ProviderController {
@@ -25,5 +28,23 @@ public class ProviderController {
         }catch (Exception e){
             return new ResponseEntity<>(e.toString(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping(path = "/provider")
+    public ResponseEntity<List<Provider>>getAllProviders(){
+        List<Provider> providers = convertIterableToList( providerRepo.findAll() );
+        if(providers.isEmpty()){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(providers, HttpStatus.OK);
+        }
+    }
+
+    private List<Provider> convertIterableToList(Iterable<Provider> iterable){
+        List<Provider> list = new ArrayList<>();
+        for(Provider provider: iterable){
+            list.add(provider);
+        }
+        return list;
     }
 }
