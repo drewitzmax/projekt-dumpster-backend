d3976e22e908ea364f48d8b72d8befcc613d5682
drewitzmax <drwitz.max@googlemail.com>
2020-08-12 14:46:24 +0200

    getOwnOrders function

--- src/main/java/com/cf/skipdiving/jpa/crud/OfferRepository.java
+++ src/main/java/com/cf/skipdiving/jpa/crud/OfferRepository.java
@@ -1,9 +1,12 @@
 package com.cf.skipdiving.jpa.crud;
 
 import com.cf.skipdiving.jpa.entity.Offer;
+import com.cf.skipdiving.jpa.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 public interface OfferRepository extends CrudRepository<Offer, BigInteger> {
+    public List<Offer> findByClaimersContains(User user);
 }
